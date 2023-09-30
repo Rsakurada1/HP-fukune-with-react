d59e0658d782f8f57f04e70a5a5421468b5f41a3
Rikito Sakurada <rikito82k@gmail.com>
2023-10-01 00:32:36 +0900

    Routes作成完了

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Home from './Components/Home';
+import Lunch from './Components/Lunch';
+import Dinner from './Components/Dinner';
+import Access from './Components/Access';
+import Introduction from './Components/Introduction';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Lunch" element={<Lunch />}></Route>
+        <Route path="/Dinner" element={<Dinner />}></Route>
+        <Route path="/Introduction" element={<Introduction />}></Route>
+        <Route path="/Access" element={<Access />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
