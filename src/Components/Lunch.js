f0ce692aadd120e171a596698cda30df502486b6
Rikito Sakurada <rikito82k@gmail.com>
2023-10-05 20:58:12 +0900

    写真のサイズ決定権各コンポーネントネーム決め

--- src/Components/Lunch.js
+++ src/Components/Lunch.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./Lunch.css"
 import Footer from "./Footer"
+import lunchi1 from "../fukune-photo/lunch1.jpg"
+import lunchi2 from "../fukune-photo/lunch2.jpg"
+import lunchi3 from "../fukune-photo/lunch3.jpg"
 
 const Lunch = () => {
   return (
-    <div>
+    <div className='lunch'>
       <div className='lunch-top'>
         <h2 className='lunch-title'>昼下がりの極上寿司体験 </h2>
         <p className='lunch-message'>
@@ -13,16 +16,22 @@ const Lunch = () => {
         それぞれのコースには、季節ごとの旬の食材と、職人の技が織り成す絶品のお寿司が揃っています。<br />
         ※季節と仕入れ状況によってメニューが変更することがございます。予めご了承ください。
         </p>
+        </div>
         <div className='lunch1'>
-
+          <h3 className='lunchi1h'></h3>
+          <img className='lunchi1i' src={lunchi1}/>
+          <p className='lunchi1p'></p>
         </div>
         <div className='lunch2'>
-
+        <h3 className='lunchi2h'></h3>
+          <img className='lunchi2i' src={lunchi2}/>
+          <p className='lunchi2p'></p>
         </div>
         <div className='lunch3'>
-          
+        <h3 className='lunchi3h'></h3>
+          <img className='lunchi3i' src={lunchi3}/>
+          <p className='lunchi3p'></p>
         </div>
-      </div>
       <Footer />
     </div>
   )
