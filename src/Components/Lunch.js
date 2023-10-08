dbd19e2638450291146f2aaba7949f9382ab5730
Rikito Sakurada <rikito82k@gmail.com>
2023-10-08 16:36:30 +0900

    微調整lunch

--- src/Components/Lunch.js
+++ src/Components/Lunch.js
@@ -58,7 +58,7 @@ const Lunch = () => {
         <div className='lunchi1' ref={contentRefs.current[0]}>
           <h2 className='lunchi1h'>『雅』-Miyabi-</h2>
           <h3 className='lunchi1-price'>1,650円</h3>
-          <p className='zei'>（税込）</p>
+          <p className='zei1'>（税込）</p>
           <h3 className='lunchi1-title'>やさしい価格で、極上の味を</h3>
           <h4 className='lunchi1-introduction'>入門となる「雅」コースでは、厳選された新鮮なネタを使用し、職人が一貫一貫丁寧に握ります。ここから始まる、寿司の旅を心ゆくまでご堪能ください。</h4>
           <img className='lunchi1i' src={lunchi1}/>
@@ -66,7 +66,7 @@ const Lunch = () => {
         <div className='lunchi2' ref={contentRefs.current[1]}>
         <h2 className='lunchi2h'>『寿』-Kotobuki-</h2>
         <h3 className='lunchi2-price'>2,200円</h3>
-        <p className='zei'>（税込）</p>
+        <p className='zei2'>（税込）</p>
         <h3 className='lunchi2-title'>バラエティに富んだ、楽しい逸品を</h3>
         <h4 className='lunchi2-introduction'>「寿」コースでは、基本のラインナップに加え、さらにバラエティ豊かな逸品をご堪能いただけます。季節の食材を活かした、彩り鮮やかな一皿をお楽しみください。</h4>
           <img className='lunchi2i' src={lunchi2}/>
@@ -74,7 +74,7 @@ const Lunch = () => {
         <div className='lunchi3' ref={contentRefs.current[2]}>
         <h2 className='lunchi3h'>『特上』-Tokujou-</h2>
         <h3 className='lunchi3-price'>4,600円</h3>
-        <p className='zei'>（税込）</p>
+        <p className='zei3'>（税込）</p>
         <h3 className='lunchi3-title'>究極の贅沢、一期一会の味わいを</h3>
         <h4 className='lunchi3-introduction'>最上級コース「特上」では、最高品質の食材を惜しみなく使用し、寿司職人の技が煌めく極上の味わいを提供いたします。特別な時間を、特別なお寿司と共に。</h4>
           <img className='lunchi3i' src={lunchi3}/>
