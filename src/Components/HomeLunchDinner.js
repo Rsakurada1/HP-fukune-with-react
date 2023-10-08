2df431fe74c6e2b301fbf681d00d564e9d4903c2
Rikito Sakurada <rikito82k@gmail.com>
2023-10-08 23:59:58 +0900

    ホーム、店舗紹介のレスポンシブ対応完了

--- src/Components/HomeLunchDinner.js
+++ src/Components/HomeLunchDinner.js
@@ -7,14 +7,14 @@ import { Link } from 'react-router-dom'
 const HomeLunchDinner = () => {
   return (
     <div className='lunch-dinner'>
-      <Link to="/Lunch">
+      <Link to="/Lunch" className='lLink'>
       <div className='home-lunch'>
         <h3 className='home-lunch-title'>～ランチ～</h3>
         <img className='home-lunch-image' src={homeLunch} />
         <p className='home-lunch-p'>　　雅、寿、特上<br/>　　　　三つのコースをご用意しております。</p>
       </div>
       </Link>
-      <Link to="/Dinner">
+      <Link to="/Dinner" className='dLink'>
         <div className='home-dinner'>
           <h3>～ディナー～</h3>
           <img className='home-dinner-image' src={homeDinner}/>
