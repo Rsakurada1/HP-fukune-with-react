7d6dd16842dd975ab9b428919986d4b7d0db31be
Rikito Sakurada <rikito82k@gmail.com>
2023-10-10 22:44:19 +0900

    バグ修正

--- src/Components/HomeLunchDinner.js
+++ src/Components/HomeLunchDinner.js
@@ -9,14 +9,14 @@ const HomeLunchDinner = () => {
     <div className='lunch-dinner'>
       <Link to="/Lunch" className='lLink'>
       <div className='home-lunch'>
-        <h3 className='home-lunch-title'>～ランチ～</h3>
+        <h3 className='home-lunch-title'>ランチ</h3>
         <img className='home-lunch-image' src={homeLunch} />
-        <p className='home-lunch-p'>　　雅、寿、特上<br/>　　　　三つのコースをご用意しております。</p>
+        <p className='home-lunch-p'>　　雅・寿・特上<br/>　　　　三つのコースをご用意しております。</p>
       </div>
       </Link>
       <Link to="/Dinner" className='dLink'>
         <div className='home-dinner'>
-          <h3>～ディナー～</h3>
+          <h3>ディナー</h3>
           <img className='home-dinner-image' src={homeDinner}/>
           <p className='home-dinner-p'>　　　旬を知る、美味を知る。</p>
         </div>
