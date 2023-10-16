a7b109c2987dbc38ba7cc312ca658a7460764433
Rikito Sakurada <rikito82k@gmail.com>
2023-10-16 22:38:43 +0900

    微調整

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Components/Home';
 import Lunch from './Components/Lunch';
 import Dinner from './Components/Dinner';
